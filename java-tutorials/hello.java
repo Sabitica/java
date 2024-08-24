0fc6fb1a719f884e1779643172c2c959c45fa534
Sabitica <sarahlecornec123+1@gmail.com>
2024-08-24 11:58:43 +0100

    first class, first function

---
+++ java-tutorials/hello.java
@@ -0,0 +1,14 @@
+class Hello 
+{
+
+    // on command line, run "java Hello" to run the below code
+    public static void main(String a[]) 
+    {
+        System.out.println("Hello World");
+    }
+
+}
+
+
+
+
