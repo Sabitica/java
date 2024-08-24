28212167bbf2cc5f3ea98ec69f8013249cde85bf
Sabitica <sarahlecornec123+1@gmail.com>
2024-08-24 12:21:09 +0100

    variables

--- java-tutorials/hello.java
+++ java-tutorials/hello.java
@@ -4,7 +4,13 @@ class Hello
     // on command line, run "java Hello" to run the below code
     public static void main(String a[]) 
     {
-        System.out.println("Hello World");
+        int num1 = 3;
+        int num2 = 2;
+        int result = num1 + num2;
+        System.out.println("Hello World"); // println new line, print is on the same line
+        System.out.println(num1);
+        System.out.println(num1 + num2);
+        System.out.println(result);
     }
 
 }
